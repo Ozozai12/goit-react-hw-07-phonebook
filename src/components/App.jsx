ad8ffae7c083fe749e89fb06d0a3009b09f7eef3
Artem Karpenko <dante120591@gmail.com>
2022-11-28 11:49:21 +0200

    donedone

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { nanoid } from 'nanoid';
 import { ContactForm } from 'components/ContactForm/ContactForm';
 import Filter from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
